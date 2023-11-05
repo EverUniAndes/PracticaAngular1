8266e9b68bae65a55c25859866004db54c8d14d9
EverUniAndes <eo.martinez2@uniandes.edu.co>
2023-11-04 21:41:31 -0500

    pruebas unitarias

--- vivero/src/app/Plantas/planta-list/planta-list.component.spec.ts
+++ vivero/src/app/Plantas/planta-list/planta-list.component.spec.ts
@@ -2,16 +2,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
+import { HttpClientModule } from '@angular/common/http';
 import { PlantaListComponent } from './planta-list.component';
+import { Planta } from '../planta';
+import { PlantasService } from '../plantas.service';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantaListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantaListComponent ],
+      providers: [ PlantasService ]
     })
     .compileComponents();
   }));
@@ -19,10 +25,37 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+      const plantas = new Planta(
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.number.int(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      );
+      component.ListaPlantas.push(plantas);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Component has a table-thead", () => {
+    expect(debug.query(By.css("thead")).childNodes.length).toBe(1);
+  });
+
+
+  it("Component has a table-tbody", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThanOrEqual(3);
+  });
+
+
+
 });
--- vivero/src/app/app.component.spec.ts
+++ vivero/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PlantaListComponent } from './Plantas/planta-list/planta-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientModule],
+    declarations: [AppComponent, PlantaListComponent]
   }));
 
   it('should create the app', () => {
@@ -19,11 +21,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('vivero');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vivero app is running!');
-  });
+  
 });
